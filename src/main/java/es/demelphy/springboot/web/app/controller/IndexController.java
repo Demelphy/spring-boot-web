6e8933fd96d2ef11da00a6d443bcb36d23f77b36
Demelphy <demel.ca.2191@gmail.com>
2022-04-27 06:34:39 +0200

    Improving Index and IndexController with Thymeleaf use

--- src/main/java/es/demelphy/springboot/web/app/controller/IndexController.java
+++ src/main/java/es/demelphy/springboot/web/app/controller/IndexController.java
@@ -1,13 +1,15 @@
 package es.demelphy.springboot.web.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
 	@GetMapping({"/index", "/", "home"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("title", "Hello from Spring Controller!");
 		return "index";
 	}
 }
