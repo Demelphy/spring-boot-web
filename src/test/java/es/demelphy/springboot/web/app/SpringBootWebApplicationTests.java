5fb028584fc53d73e6777d9fa9d68b6d02a0870f
Demelphy <demel.ca.2191@gmail.com>
2022-04-27 05:31:56 +0200

    First Commit

---
+++ src/test/java/es/demelphy/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.demelphy.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
