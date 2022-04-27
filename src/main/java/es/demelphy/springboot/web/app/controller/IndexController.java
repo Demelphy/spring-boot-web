11ded34c528eef8f19a5e05e04fbe06c0b7cf289
Demelphy <demel.ca.2191@gmail.com>
2022-04-27 07:16:34 +0200

    Improving GetMapping for index

--- src/main/java/es/demelphy/springboot/web/app/controller/IndexController.java
+++ src/main/java/es/demelphy/springboot/web/app/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping({"/index", "/", "home"})
+	@GetMapping({"/index", "/", "", "home"})
 	public String index(Model model) {
 		model.addAttribute("title", "Hello from Spring Controller!");
 		return "index";
