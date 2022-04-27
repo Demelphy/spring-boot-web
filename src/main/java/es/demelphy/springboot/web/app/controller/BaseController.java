da25536666e0d2893f48c7c797336a89a41847f9
Demelphy <demel.ca.2191@gmail.com>
2022-04-27 09:15:49 +0200

    Adding CurrentDate in all pages

--- src/main/java/es/demelphy/springboot/web/app/controller/BaseController.java
+++ src/main/java/es/demelphy/springboot/web/app/controller/BaseController.java
@@ -1,9 +1,23 @@
 package es.demelphy.springboot.web.app.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class BaseController {
 
-	
+	@ModelAttribute("currentDate")
+	public String getCurrentDate() {
+		Date date = Calendar.getInstance().getTime();
+
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+	    String today = formatter.format(date);
+
+		return today;
+	}
 }
