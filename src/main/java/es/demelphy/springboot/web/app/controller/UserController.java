649a48e81964e0a7515c6d5b560d766807ea7ccf
Demelphy <demel.ca.2191@gmail.com>
2022-04-27 08:50:38 +0200

    Improving User Info

--- src/main/java/es/demelphy/springboot/web/app/controller/UserController.java
+++ src/main/java/es/demelphy/springboot/web/app/controller/UserController.java
@@ -1,5 +1,8 @@
 package es.demelphy.springboot.web.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,11 @@ public class UserController {
 	@RequestMapping
 	public String getUser(Model model) {
 		User user = new User("Demel", "Master Phy");
-		model.addAttribute("title", "User Info");
+		user.setEmail("demelmasterphy@mail.com");
+		
+		model.addAttribute("title", "User Info - "+user.getFirstName() + " " + user.getLastName());
 		model.addAttribute("user", user);
+		
 		return "/users/user";
 	}
 }
